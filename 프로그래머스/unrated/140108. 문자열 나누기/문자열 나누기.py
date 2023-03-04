7d79b8dbd9671cf9f19b37a2d2115b7ea2801e1d
Minji Park <46767966+arislid@users.noreply.github.com>
2023-03-04 13:03:20 +0900

    [unrated] Title: 문자열 나누기, Time: 4.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
@@ -0,0 +1,22 @@
+def solution(s):
+    answer = ''
+    ans = []
+    for idx, letter in enumerate(s):
+        if len(answer) == 0:
+            first = s[idx]
+            answer = first
+            if idx == len(s) - 1:
+                ans.append(answer)
+            
+        else:
+            answer += letter
+            if int(len(answer) // 2) == answer.count(first):
+                ans.append(answer)
+                answer = ''
+            elif idx == len(s) - 1:
+                ans.append(answer)
+
+
+                
+        
+    return len(ans)
