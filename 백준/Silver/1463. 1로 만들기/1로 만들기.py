e9ec2f2812ad9ca9ee618d5bd5f3119126065965
Minji Park <46767966+arislid@users.noreply.github.com>
2023-04-27 07:32:51 +0900

    [Silver III] Title: 1로 만들기, Time: 340 ms, Memory: 39504 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,13 +1,16 @@
-def makeone(n):
-    dp = [0] * (n + 1)
-    for i in range(2, n+1):
-        dp[i] = dp[i-1] + 1
-        if i % 3 == 0:
-            dp[i] = min(dp[i], dp[i//3]+ 1)
-        if i % 2 == 0:
-            dp[i] = min(dp[i], dp[i//2]+ 1)
-    return dp[n]
+def makeone_review():
+    X = int(input())
+    dp = [0 for _ in range(X+1)]
 
-n = int(input())
+    for i in range(2, X+1):
+        if i % 6 == 0:
+            dp[i] = min(dp[i//2], dp[i//3]) + 1
+        elif i % 2 == 0:
+            dp[i] = min(dp[i-1], dp[i//2]) + 1
+        elif i % 3 == 0:
+            dp[i] = min(dp[i-1], dp[i//3]) + 1
+        else:
+            dp[i] = dp[i-1] + 1
+    print(dp[X])
 
-print(makeone(n))
+makeone_review()
