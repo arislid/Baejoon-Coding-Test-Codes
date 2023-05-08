6555a07c68601b01767ef23d9e2ce0cdded19ddd
Minji Park <46767966+arislid@users.noreply.github.com>
2023-05-08 09:06:25 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,11 @@
+def tiling_2n():
+    n = int(input())
+    dp = [0] * 1001
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    
+    print(dp[n] % 10007)
+    
+tiling_2n()
