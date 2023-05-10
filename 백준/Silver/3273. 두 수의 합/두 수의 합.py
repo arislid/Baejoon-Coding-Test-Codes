8baa562a36c51ae4ad8b39aaba1690d346d72f31
Minji Park <46767966+arislid@users.noreply.github.com>
2023-05-10 14:47:52 +0900

    [Silver III] Title: 두 수의 합, Time: 88 ms, Memory: 42172 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,22 +1,26 @@
-from collections import Counter
-
-def find_pairs(sequence, x):
+# O(n)의 시간복잡도를 고려해서 for문 한 번 만으로도 경우의 수 셀 수 있도록 짤 것
+def two_pointer(sequence, x):
     count = 0
-    counter = Counter(sequence)
-
-    for num in counter:
-        complement = x - num
-        if complement in counter and complement != num:
-            count += counter[num] * counter[complement]
-
-    return count // 2
+    start = 0
+    end = len(sequence) - 1
+    sequence.sort()
+    while start < end:
+        if sequence[start] + sequence[end] == x:
+            count += 1
+            start += 1
+            end -= 1
+        elif sequence[start] + sequence[end] < x:
+            start += 1
+        else:
+            end -= 1
+    return count
 
 def main():
     n = int(input())
     sequence = list(map(int, input().split()))
     x = int(input())
 
-    result = find_pairs(sequence, x)
+    result = two_pointer(sequence, x)
     print(result)
 
 if __name__ == "__main__":
